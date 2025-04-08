e7cd6cb1326b9247d8a030296e1955c457976ccf
Chris Toribio <44413949+cromtoribio@users.noreply.github.com>
2025-04-07 20:57:33 -0700

    Updated Blog URL

--- src/components/interface/Footer.jsx
+++ src/components/interface/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
                 <li><a href="https://github.com/cromtoribio/3d-portfolio">GITHUB</a></li>
                 <li><a href="https://www.instagram.com/christoribio">INSTAGRAM</a></li>
                 <li><a href="mailto:hello@christoribio.com">EMAIL</a></li>
-                <li><a href="https://cromtoribio.github.io/thealchemyjournal/">BLOG</a></li>
+                <li><a href="https:/journal.christoribio.com">BLOG</a></li>
             </ul>
         </footer>
     </div>
